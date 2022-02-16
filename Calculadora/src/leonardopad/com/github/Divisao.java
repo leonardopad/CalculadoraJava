3ef2c29934f036320b4ec3f92caaa8c75566ea67
leonardopad <77343517+leonardopad@users.noreply.github.com>
2022-02-15 22:24:09 -0300

    Add files via upload

---
+++ Calculadora/src/leonardopad/com/github/Divisao.java
@@ -0,0 +1,14 @@
+package leonardopad.com.github;
+
+public class Divisao {
+	public static void main(String[] args) {
+		Integer numero1 = Integer.parseInt(args[0]);
+		Integer numero2 = Integer.parseInt(args[1]);
+		Integer resultado = numero1 / numero2;
+		System.out.println("divisao: " + resultado);
+		//System.out.println("soma: " + resultado.toOctalString(resultado));
+		//System.out.println("soma: " + resultado.toBinaryString(resultado));
+		//System.out.println("soma: " + resultado.toHexString(resultado));
+		
+	}
+}
---
+++ Calculadora/src/leonardopad/com/github/Soma.java
@@ -0,0 +1,16 @@
+package leonardopad.com.github;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		Integer numero1 = Integer.parseInt(args[0]);
+		Integer numero2 = Integer.parseInt(args[1]);
+		Integer resultado = numero1 + numero2;
+		System.out.println("soma: " + resultado);
+		//System.out.println("soma: " + resultado.toOctalString(resultado));
+		//System.out.println("soma: " + resultado.toBinaryString(resultado));
+		//System.out.println("soma: " + resultado.toHexString(resultado));
+		
+	}
+
+}
